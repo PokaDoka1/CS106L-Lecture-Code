e58b2584bcd3a54a17e27d50e0d9c47bec14423d
PokaDoka1 <90730806+PokaDoka1@users.noreply.github.com>
2025-11-13 13:31:29 -0800

    Add compile and run instructions to practice.cpp
    Added compilation and execution instructions for practice.cpp.

---
+++ practice.cpp
@@ -0,0 +1,21 @@
+/*
+ * Credit to Sarah McCarthy '23 for this example
+ */
+
+//compile with: g++ -std=c++20 practice.cpp -o practice
+//run with: ./practice (on mac)
+
+#include <iostream>
+#include <optional>
+
+std::optional<int> divide(int numerator, int denominator) {
+    /* FILL IN HERE */
+}
+
+int main() {
+  /* CALL HERE WITH (10, 2)*/
+  
+  /* CALL HERE WITH (10, 0)*/
+
+  return 0;
+}
